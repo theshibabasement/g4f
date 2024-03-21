af5297508005499b26ecbe99ee154588c55579d6
theshibabasement <92958705+theshibabasement@users.noreply.github.com>
2024-03-21 09:09:07 -0300

    Update run.py

--- g4f/api/run.py
+++ g4f/api/run.py
@@ -3,4 +3,4 @@ import g4f.api
 
 if __name__ == "__main__":
     print(f'Starting server... [g4f v-{g4f.version.utils.current_version}]')
-    g4f.api.Api(engine = g4f, debug = True).run(ip = "0.0.0.0:10000")
+    g4f.api.Api(engine = g4f, debug = True).run(host="gpt4free.limemarketing.online", port=10000)
